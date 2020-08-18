33b4182bcb2c8ce02c2290728cff42cf0f3f26d5
Efim Bakulin <efimbakulin@gmail.com>
2020-08-18 15:35:50 +0300

    improvements

--- src/player/render.py
+++ src/player/render.py
@@ -4,4 +4,4 @@ from utils import parse_show_time
 def render_show_time(show_descr):
     fromTs = parse_show_time(show_descr['start'])
     toTs = parse_show_time(show_descr['stop'])
-    return f"{fromTs.strftime('%H:%M')} - {toTs.strftime('%H:%M')}"
+    return f"{fromTs.strftime('%d.%m.%y')} {fromTs.strftime('%H:%M')} - {toTs.strftime('%H:%M')}"
